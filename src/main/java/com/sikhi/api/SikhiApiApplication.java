847448dfa86a246681c84d8ad6c371c434786501
harpender <harpender@gmail.com>
2017-02-12 16:17:52 +0000

    added initial project

---
+++ src/main/java/com/sikhi/api/SikhiApiApplication.java
@@ -0,0 +1,12 @@
+package com.sikhi.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SikhiApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SikhiApiApplication.class, args);
+	}
+}
